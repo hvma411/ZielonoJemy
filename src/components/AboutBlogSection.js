c55dbf1dbe6fec3e3dd85977cbde690a5d88a7b1
hvma411 <kamil.nosek411@gmail.com>
2020-11-28 15:04:54 +0100

    some repairs in text

--- src/components/AboutBlogSection.js
+++ src/components/AboutBlogSection.js
@@ -12,7 +12,7 @@ const AboutBlogSection = () => {
     return (
         <section className="about-blog-section">
           <div className="text-part-box container">
-            <h2>Dlaczego zielonojemy?</h2>
+            <h2>Dlaczego Zielonojemy?</h2>
             <div className="under-line"></div>
             <h3>Mam dwie edukacyjne misje w życiu. Pierwszą jest zachęcanie do świadomych wyborów żywieniowych, abyśmy jako społeczeństwo mogli cieszyć się zdrowiem jak najdłużej. Drugą natomiast uświadamianie, że to, co jemy ma realny wpływ na naszą planetę. Jedząc bardziej roślinnie, możemy rozświetlić jej przyszłość!</h3>
           </div>
--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -20,7 +20,7 @@ const AboutMeSection = () => {
                     <div className="medium-line"></div>
                     <h2>Cześć, mam na imię Karolina</h2>
                     <h4>Jestem dietetykiem klinicznym, miłośniczką dobrej kawy i fanką diety roślinnej.</h4>
-                    <p>Dyplom uzyskałam na Wydziale Lekarskim Collegium Medicum Uniwersytetu Jagiellońskiego w Krakowie. Dietetyką zaczęłam interesować się już w liceum, czyli w tym roku minie 6 lat, odkąd przeczytałam pierwsze podręczniki z nią związane. Chciałabym Ci pokazać, że zbilansowana dieta i aktywność fizyczna to droga do zdrowia i dobrego samopoczucia. Co ważne - nie potrzebujesz do tego garści suplementów, diety cud czy detoksów.
+                    <p>&nbsp; Dyplom uzyskałam na Wydziale Lekarskim Collegium Medicum Uniwersytetu Jagiellońskiego w Krakowie. Dietetyką zaczęłam interesować się już w liceum, czyli w tym roku minie 6 lat, odkąd przeczytałam pierwsze podręczniki z nią związane. Chciałabym Ci pokazać, że zbilansowana dieta i aktywność fizyczna to droga do zdrowia i dobrego samopoczucia. Co ważne - nie potrzebujesz do tego garści suplementów, diety cud czy detoksów.
                     Jaką dietetykę popularyzuję? Opartą na faktach naukowych w przystępnej formie, abyś bez wykształcenia okołomedycznego czuł/a ten klimat.
                     </p>
                     <Link to="/about">
