6f8a2448bae3962e3fe70c409750fb2cbdcd22e3
hvma411 <kamil.nosek411@gmail.com>
2021-01-03 16:39:14 +0100

    contact page mobile and dekstop done, a few small repairs done

--- src/components/MenuComponents/Contact.js
+++ src/components/MenuComponents/Contact.js
@@ -8,11 +8,144 @@ import {
   NavLink,
 } from 'react-router-dom';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookSquare, faInstagramSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+
+
 const Contact = () => {
+
+    const [contactForm, setContactForm] = useState({
+        fullName: "",
+        emailAddress: "",
+        guestMessage: "",
+    });
+
+    const validateEmail = (email) => {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    const sendMessage = () => {
+        const errors = [];
+
+        if (contactForm.fullName === "" || contactForm.fullName.length < 2) {
+            errors.push("wrongName");
+            document.querySelector(".wrongName").style.display = "block";
+        };
+
+        if (validateEmail(contactForm.emailAddress) === false) {
+            errors.push("wrongEmail");
+            document.querySelector(".wrongEmail").style.display = "block";
+        };
+
+        if (contactForm.guestMessage.length < 30) {
+            errors.push('wrongMessage');
+            document.querySelector(".wrongMessage").style.display = "block";
+        };
+
+        if (contactForm.fullName === "" && contactForm.emailAddress === "" && contactForm.guestMessage === "") {
+            errors.push("emptyForm");
+            document.querySelector(".emptyForm").style.display = "block";
+            document.querySelector(".wrongName").style.display = "none";
+            document.querySelector(".wrongEmail").style.display = "none";
+            document.querySelector(".wrongMessage").style.display = "none";
+        };
+
+        if (errors.length < 1) {
+            document.querySelector(".messageInfo").style.display = "block";
+
+            const classClear = setTimeout(() => {
+                document.querySelector(".messageInfo").style.display = "none";
+            }, 1500);
+        };
+        
+    };
+    
+
+    const handleFormChange = (e) => {
+        e.persist();
+        setContactForm(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleButtonClick = (e) => {
+        e.preventDefault();
+        sendMessage();
+    }
+
+
+    useEffect(() => {
+        if (contactForm.fullName.length > 2) {
+            document.querySelector(".wrongName").style.display = "none";
+        };
+
+        if (validateEmail(contactForm.emailAddress) === true) {
+            document.querySelector(".wrongEmail").style.display = "none";
+        };
+
+        if (contactForm.guestMessage.length > 29) {
+            document.querySelector(".wrongMessage").style.display = "none";
+        }
+        
+        if (contactForm.fullName.length > 0 || contactForm.emailAddress.length > 0 || contactForm.guestMessage.length > 0) {
+            document.querySelector(".emptyForm").style.display = "none";
+        }
+    }, [contactForm])
+
+    
     return (
-        <section className="articles">
-            <div className="container">
-                CONTACT PAGE
+        <section className="contact">
+            <div className="container contact__page">
+                <h3 className="section__title">SKONTAKTUJ SIĘ ZE MNĄ</h3>
+                <span className="underline"></span>
+                <div className="page-box">
+                    <div className="form-box">
+                        <h4>Moje dane kontaktowe:</h4>
+                        <div className="contact__details">
+                            <div className="detail-box">
+                                <h5>TELEFON:</h5>
+                                <p><a href="tel:+48789455613">+48 789 455 613</a></p>
+                            </div>
+                            <div className="detail-box">
+                                <h5>EMAIL:</h5>
+                                <p><a href="mailto:furmanska.diet@gmail.com">furmanska.diet@gmail.com</a></p>
+                            </div>
+                        </div>
+                        <h4>Moje socialmedia:</h4>
+                        <div className="socialmedia">
+                            <a href="https://www.facebook.com/Zielonojemy-Karolina-Furma%C5%84ska-101969841692612" target="blank_">
+                                <FontAwesomeIcon icon={ faFacebookSquare } />
+                            </a>
+                            <a href="https://www.instagram.com/zielonojemy/?hl=pl" target="blank_">
+                                <FontAwesomeIcon icon={ faInstagramSquare }/>
+                            </a>
+                                <FontAwesomeIcon icon={ faTwitterSquare }/>
+                        </div>
+                        <form>
+                            <h4>Napisz do mnie wiadomość:</h4>
+                            <input type="text" name="fullName" value={ contactForm.fullName } onChange={ handleFormChange } placeholder="Twoje imie..." />
+                            <input type="text" name="emailAddress" value={ contactForm.emailAddress } onChange={ handleFormChange } placeholder="Twój adres email..." />
+                            <textarea name='guestMessage' value={ contactForm.guestMessage } onChange={ handleFormChange } placeholder="Twoja wiadomość..." />
+                            <div className="button-box">
+                                <div className="errors-box">
+                                    <span className="wrongName">Podaj proszę swoje imię</span>
+                                    <span className="wrongEmail">Popraw adres email</span>
+                                    <span className="wrongMessage">Twoja wiadomość jest za krótka (min. 30 znaków)</span>
+                                    <span className="emptyForm">Uzupełnij poprawnie wszystkie pola (wiadomość to min. 30 znaków)</span>
+                                    <span className="messageInfo">Wiadomość została wysłana</span>
+                                </div>
+                                <button onClick={ handleButtonClick }>Wyślij</button>
+                            </div>
+
+                        </form>
+
+                    </div>
+                    <div className="img-box">
+                        
+                    </div>
+                </div>
             </div>
         </section>
     );
