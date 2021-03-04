fd89866bd0fe8555f7ee2276a3be91fc40aa9c25
hvma411 <kamil.nosek411@gmail.com>
2021-03-04 21:32:12 +0100

    search engine another part

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -10,15 +10,28 @@ import {
 import firebase from '../config/firebase';
 import parse from 'html-react-parser';
 
-const SearchEngine = () => {
+const SearchEngine = ({ tagToSearch, setTagToSearch }) => {
+
+    const [tag, setTag] = useState('')
+
+    const handleFormChange = (e) => {
+        e.persist();
+        setTag(e.target.value)
+    };
+
+    const handleSearchClick = (e) => {
+        e.preventDefault()
+        setTagToSearch(tag)
+    }
+
     return (
         <div className="search-engine-box">
             <form className="search-form">
                 <div className="input-box">
                     <label htmlFor="search-engine" className="hash">#</label>
-                    <input id="search-engine" type="text" placeholder="Wpisz szukaną frazę..." />
+                    <input id="search-engine" onChange={ handleFormChange } value={ tag } type="text" placeholder="Wpisz szukaną frazę..." />
                 </div>
-                <button className="search-btn">Szukaj</button>
+                <button onClick={ handleSearchClick } className="search-btn">Szukaj</button>
             </form>
         </div>
     )
--- src/components/SearchResultModal.js
+++ src/components/SearchResultModal.js
@@ -1,7 +1,14 @@
+import { ta } from 'date-fns/locale';
 import React, { useState, useEffect } from 'react';
 import SearchEngine from './SearchEngine';
+import firebase from '../config/firebase';
+import parse from 'html-react-parser';
+
+
 
 const SearchResultModal = () => {
+
+    const db = firebase.firestore();
 
     const result = {
         src: 'http://www.pixelstalk.net/wp-content/uploads/2016/05/Photo-Images-Bing-background.jpg',
@@ -13,35 +20,88 @@ const SearchResultModal = () => {
 
     const arr = [result, result, result, result, result, result]
 
+    const [tagToSearch, setTagToSearch] = useState('')
+    const [documents, setDocuments] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    async function getResultDocuments() {
+
+        const recipesRef = await db.collection('articlesAndRecipes').doc('allPosts').collection('recipes');
+        const articlesRef = await db.collection('articlesAndRecipes').doc('allPosts').collection('articles');
+        
+        let documentsArr = [];
+
+        await recipesRef.where('hashTags', 'array-contains', '#' + tagToSearch).get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                documentsArr.push({ id: doc.id, ...doc.data() })
+            });
+        })
+        .catch((error) => {
+            setLoading(true);
+            console.error("Error while getting documents: ", error);
+        });
+        
+        if (documentsArr.length > 0) {
+            setDocuments(documentsArr);
+        }
+        // setLoading(false);
+
+        await articlesRef.where('hashTags', 'array-contains', '#' + tagToSearch).get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                documentsArr.push({ id: doc.id, ...doc.data() })
+            });
+        })
+        .catch((error) => {
+            setLoading(true);
+            console.error("Error while getting documents: ", error);
+        });
+
+        if (documentsArr.length > 0) {
+            setDocuments(documentsArr);
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getResultDocuments()
+        console.log(tagToSearch)
+        console.log(documents)
+    }, [tagToSearch])
+
     return (
         <div className="search-result-background">
             <div className="search-result-modal">
                 <div className="list-box">
                     <ul className="result-list">
-                        { arr.map((obj, idx) => (
+                        { documents != null ? documents.map((obj, idx) => (
                             <SingleSearchResult key={idx} result={ obj } />
-                        ))}
+                        )) : null }
                         {/* <SingleSearchResult result={ result } /> */}
                     </ul>
                 </div>
-                <SearchEngine />
+                <div className="space-wrapper"></div>
+                <SearchEngine tagToSearch={ tagToSearch } setTagToSearch={ setTagToSearch } />
             </div>
         </div>
     )
 }
 
 const SingleSearchResult = ({ result }) => {
+
+    console.log(result)
+
     return (
         <li className="single-search-result">
-            <img className="result-img" src={ result.src } />
+            <img className="result-img" src={ result.featureImage } />
             <div className="result-details">
                 <div className="title-row">
                     <h3>{ result.title }</h3>
                     <span>{ result.date }</span>
                 </div>
-                <div className="content-row">{ result.content }</div>
+                <div className="content-row">{ parse(result.content.slice(0, 97) + "...")}</div>
                 <ul className="tags-row">
-                    { result.tags.map((value, idx) => (
+                    { result.hashTags.map((value, idx) => (
                         <li key={ idx }>{ value }</li>
                     )) }
                 </ul>
